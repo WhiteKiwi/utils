53d36bb23e7f4a493f3a24391d0829860089c0da
whitekiwi <jh145478@gmail.com>
2021-12-03 23:18:19 +0900

    refactor(): function types 제네릭 리팩토링

--- src/types/function.ts
+++ src/types/function.ts
@@ -1,2 +1,38 @@
-export type Function<T extends any[], R> = (...args: T) => R;
-export type AsyncFunction<T extends any[], R> = (...args: T) => Promise<R>;
+export type Function5<A, B, C, D, E, R> = (a: A, b: B, c: C, d: D, e: E) => R;
+export type Function4<A, B, C, D, R> = (a: A, b: B, c: C, d: D) => R;
+export type Function3<A, B, C, R> = (a: A, b: B, c: C) => R;
+export type Function2<A, B, R> = (a: A, b: B) => R;
+export type Function1<A, R> = (a: A) => R;
+export type Function<
+	A,
+	B,
+	C = undefined,
+	D = undefined,
+	E = undefined,
+	F = undefined,
+> = F extends undefined
+	? E extends undefined
+		? D extends undefined
+			? C extends undefined
+				? Function1<A, B>
+				: Function2<A, B, C>
+			: Function3<A, B, C, D>
+		: Function4<A, B, C, D, E>
+	: Function5<A, B, C, D, E, F>;
+
+export type AsyncFunction<
+	A,
+	B,
+	C = undefined,
+	D = undefined,
+	E = undefined,
+	F = undefined,
+> = F extends undefined
+	? E extends undefined
+		? D extends undefined
+			? C extends undefined
+				? Function1<A, Promise<B>>
+				: Function2<A, B, Promise<C>>
+			: Function3<A, B, C, Promise<D>>
+		: Function4<A, B, C, D, Promise<E>>
+	: Function5<A, B, C, D, E, Promise<F>>;
